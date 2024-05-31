49a874137f25fc5ef4afadaaaec1c65ce3db8079
Mayuran Visakan <mayuran.k.v@gmail.com>
2024-05-31 17:02:21 +0100

    update

---
+++ Project/modules/generic_module/config.py
@@ -0,0 +1,33 @@
+# Specify exports
+__all__ = ["initialise"]
+
+# Imports
+from pathlib import Path
+
+from dotenv import load_dotenv
+from loguru import logger
+
+resolved_parents = Path(__file__).resolve().parents
+THESIS_DIRECTORY = resolved_parents[3]
+PROJECT_DIRECTORY = resolved_parents[2]
+MODULE_NAME = resolved_parents[0].name
+
+
+# Initialisation
+def initialise() -> None:
+	load_dotenv()
+
+	try:
+		from tqdm import tqdm
+
+		logger.remove(0)
+		logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
+	except ModuleNotFoundError:
+		pass
+
+	logger.info(f"Loaded module `{MODULE_NAME}` from project directory path `{PROJECT_DIRECTORY}`")
+
+
+# Calling
+if __name__ == "__main__":
+	raise RuntimeError("Config should not be run directly")
