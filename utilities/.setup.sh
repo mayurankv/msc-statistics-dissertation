7693922b37ed9503ba4de29e2f388fa798b14518
mayurankv <mayuran.k.v@gmail.com>
2024-06-11 04:44:16 +0100

    update

---
+++ utilities/.setup.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+DISSERTATION_PATH=~/Documents/Mayuran/Programming/Projects/Academic/Imperial\ College\ London/MSc\ Statistics/Dissertation
+
+ln -s $MYOBSIDIANPATH/package_settings/.latex_suite/snippets $DISSERTATION_PATH/utilities/obsidian/snippets
